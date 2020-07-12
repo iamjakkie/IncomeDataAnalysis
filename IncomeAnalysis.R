1e5928dcf2223f05a004caf55872460539e2ecb9
Jakub Kiełbasiewicz <jakubkielbasiewicz@gmail.com>
2020-07-12 17:59:26 +0200

    Added initial readme description file and started EDA

--- IncomeAnalysis.R
+++ IncomeAnalysis.R
@@ -1,1 +1,23 @@
-data <- 
+library("tidyverse")
+
+data <- read_csv('Data/adult.data.txt', col_names = c("Age", "Workclass",
+                                                      "fnlwgt","education", 
+                                                      "educational_num", 
+                                                      "marital_status", 
+                                                      "occupation", 
+                                                      "relationship", "race", 
+                                                      "sex", "capital_gain",
+                                                      "capital_loss", 
+                                                      "hours_per_week", 
+                                                      "native_country",
+                                                      "income"))
+head(data)
+
+
+data %>% filter(income==">50k") %>% count 
+
+data %>% filter(income == "<=50k") %>% count
+
+ggplot(data, aes(sex)) + geom_bar()
+gender_income <- data %>% group_by(sex) %>% count(income) 
+ggplot(gender_income, aes(sex,n)) + geom_boxplot()
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # IncomeDataAnalysis
+> Initial goal of the project is to create number of R notebooks: 
+- EDA
+- Machine learning
+- Deep learning
+for a given dataset
+
+## Dataset
+> The dataset is publicly available at [Source](https://archive.ics.uci.edu/ml/datasets/Adult). 
+It contains data for adult's income and number of parameters.
+
+## Summary of findings (EDA)
+> TODO
+
+## Algorithms
+> TODO
